4ca434f22e73afe75dfd513936fb794403cb748f
Jean <jeanlucafp@gmail.com>
2020-09-30 11:49:11 -0300

    aula 2

---
+++ kind.sh
@@ -0,0 +1,8 @@
+## usar o kind para criar o cluster
+## https://kind.sigs.k8s.io/docs/user/quick-start/
+
+kind create cluster --name meucluster
+
+kind get clusters
+
+kind delete cluster --name meucluster
