979e3419c1c87b7e28941bbdb63c0dbe8d5f6532
Jean <jeanlucafp@gmail.com>
2021-03-11 08:33:01 -0300

    fix

--- semana-2/k3d.sh
+++ semana-2/k3d.sh
@@ -5,7 +5,7 @@ k3d cluster list
 
 k3d cluster delete ${NOME_CLUSTER}
 
-k3d cluster create agora-vai --servers 1 --agents 2 -p "8080:30000@loadbalancer" -p "8181:30001@loadbalancer" -p "8282:30002@loadbalancer"
+k3d cluster create semana-kube-dev --servers 1 --agents 2 -p "8080:30000@loadbalancer" -p "8181:30001@loadbalancer" -p "8282:30002@loadbalancer"
 
 kubectl get nodes
 
